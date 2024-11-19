584ea7d55f7c4ae3fa95a9e7527be5621eb86686
Rithuik <rithuik.jsp@gmail.com>
2024-11-19 20:04:02 +0530

    added jwt default key

--- qcomm_usermgmt/src/app.py
+++ qcomm_usermgmt/src/app.py
@@ -10,7 +10,7 @@ import os
 app  =  Flask(__name__)
 
 
-key = os.getenv('JWT_KEY')
+key = os.getenv('JWT_KEY','https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c')
 
 
 @app.route("/")
