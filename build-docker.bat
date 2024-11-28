a126a701734b3bf199d003abed2a2446b6160e83
Rithuik <rithuik.jsp@gmail.com>
2024-11-28 07:14:47 +0530

    pushed updated code

--- build-docker.bat
+++ build-docker.bat
@@ -1,1 +1,4 @@
 docker build  -f Order_Managment_Dockerfile -t rithuikyerrabrolu/usermgmt:v1.0 .
+
+
+docker push rithuikyerrabrolu/usermgmt:v1.0
--- qcomm_usermgmt/src/service/DBService.py
+++ qcomm_usermgmt/src/service/DBService.py
@@ -4,12 +4,15 @@ from sqlalchemy.orm import Session,sessionmaker
 from sqlalchemy import URL
 from orm.Entity import User,UserCommunications,UserAuth
 from sqlalchemy import select
+import os
 
 
 class DBService():
 
     def __init__(self):
-        self.url = URL.create("postgresql+psycopg2",username="postgres",password="root",host="localhost",database="postgres",port="5432")
+        dbHost = os.getenv("DB_HOST","database-1.cvqgcygm0awc.ap-south-1.rds.amazonaws.com")
+        print("DB HOST "+dbHost)
+        self.url = URL.create("postgresql+psycopg2",username="postgres",password="root",host=dbHost,database="postgresdb1",port="5432")
         self.engine = engine = create_engine(self.url)
     
     def persist_obj(self,obj):
@@ -21,6 +24,7 @@ class DBService():
         data ={}
         with Session(self.engine) as session:
             data = session.execute(select(UserAuth).where(UserAuth.login==login)).first()
+            print(data)
             
         return data
 
