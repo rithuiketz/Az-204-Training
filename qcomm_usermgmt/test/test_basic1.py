b42b851bba72edbd50b006f5e53c75d4789744e2
Rithuik <rithuik.jsp@gmail.com>
2024-12-09 08:43:56 +0530

    added code changes

--- qcomm_usermgmt/test/test_basic1.py
+++ qcomm_usermgmt/test/test_basic1.py
@@ -21,10 +21,5 @@ def runner(test_app):
 
 def test_api_get_check(client):
     client_ =  client
-    resp = client_.get("/smal")
+    resp = client_.get("/envs")
     assert resp.status_code ==200
-
-def test_post_check(client):
-    client_ =  client
-    resp = client_.get("/post/123")
-    assert resp.get_data(as_text=True)  == "Post 123"
